04484f5e6e5f0f826c594c4bfcc8fbab3d1955e5
Neda <nedachangizi@gmail.com>
2022-11-22 11:05:36 -0800

    event target

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const clickMeButton = document.querySelector(".click-me");
 
-function handleClickMeButton() {
+function handleClickMeButton(event) {
+//   console.log(event.target);
   alert("You Clicked!");
   // clickMeButton.removeEventListener("click", handleClickMeButton);
 }
@@ -16,12 +17,28 @@ clickMeButton.addEventListener("click", changeBGColor);
 function changeText() {
   if (clickMeButton.textContent === "Click Me!") {
     clickMeButton.textContent = "Clicked";
-  }
-  else if (clickMeButton.textContent === "Clicked")
-  {
+  } else if (clickMeButton.textContent === "Clicked") {
     clickMeButton.textContent = "Click Me!";
-
   }
 }
 
 clickMeButton.addEventListener("click", changeText);
+
+function createAddButton() {
+  const purpleButton = document.createElement("button");
+  purpleButton.textContent = "Purple";
+  purpleButton.addEventListener("mouseover", changeBGGreen);
+  document.body.appendChild(purpleButton);
+}
+clickMeButton.addEventListener("click", createAddButton);
+function createAddPara() {
+  const para = document.createElement("p");
+  para.textContent = "My para";
+  para.addEventListener("mouseover", changeBGGreen);
+  document.body.appendChild(para);
+}
+clickMeButton.addEventListener("click", createAddPara);
+
+function changeBGGreen(event) {
+  event.target.classList.add("green-background")
+}
