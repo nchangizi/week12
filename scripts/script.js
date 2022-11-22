28e3b680dd802785a50bc1574f4e437ffe23b8ef
Neda <nedachangizi@gmail.com>
2022-11-22 10:39:18 -0800

    add more event listener

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,27 @@
 const clickMeButton = document.querySelector(".click-me");
 
-function handleClickMeButton ()
-{
-    alert("You Clicked!");
-    // clickMeButton.removeEventListener("click", handleClickMeButton);
+function handleClickMeButton() {
+  alert("You Clicked!");
+  // clickMeButton.removeEventListener("click", handleClickMeButton);
 }
 
-clickMeButton.addEventListener("click", handleClickMeButton, {once:true});
+clickMeButton.addEventListener("click", handleClickMeButton);
 
+function changeBGColor() {
+  // document.body.style.backgroundColor = "pink";
+  document.body.classList.add("pink");
+}
+clickMeButton.addEventListener("click", changeBGColor);
+
+function changeText() {
+  if (clickMeButton.textContent === "Click Me!") {
+    clickMeButton.textContent = "Clicked";
+  }
+  else if (clickMeButton.textContent === "Clicked")
+  {
+    clickMeButton.textContent = "Click Me!";
+
+  }
+}
+
+clickMeButton.addEventListener("click", changeText);
