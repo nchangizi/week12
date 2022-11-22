b43db1fc79146db09e9b8b41fa88988104637242
Neda <nedachangizi@gmail.com>
2022-11-22 10:22:47 -0800

    remove listener

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,8 @@ const clickMeButton = document.querySelector(".click-me");
 function handleClickMeButton ()
 {
     alert("You Clicked!");
+    // clickMeButton.removeEventListener("click", handleClickMeButton);
 }
 
-clickMeButton.addEventListener("click", handleClickMeButton);
+clickMeButton.addEventListener("click", handleClickMeButton, {once:true});
+
